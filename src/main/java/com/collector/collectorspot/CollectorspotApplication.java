5fa79f0d388f39d990f1ecd203af56624d7ad326
GrayChiang <graychiang@bluetechnology.com.tw>
2024-03-27 17:15:43 -0400

    Initial commit

---
+++ src/main/java/com/collector/collectorspot/CollectorspotApplication.java
@@ -0,0 +1,13 @@
+package com.collector.collectorspot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CollectorspotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CollectorspotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/collector/collectorspot/CollectorspotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.collector.collectorspot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CollectorspotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
