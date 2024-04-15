18fd86e19dc7023b3f211dc5ac1ec84694db9689
Hello <cesstevenmarty@gmail.com>
2024-04-14 19:49:54 -0500

    Order Service CRUD

--- OrderService/src/main/java/com/collector/orderservice/OrderServiceImpl.java
+++ OrderService/src/main/java/com/collector/orderservice/OrderServiceImpl.java
@@ -3,13 +3,39 @@ package com.collector.orderservice;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.Optional;
+
 @Service
-public class OrderServiceImpl {
+public class OrderServiceImpl{
 //    @Autowired
     private final CartRepository cartRepository;
-//    @Autowired
+    @Autowired
     private final OrderRepository orderRepository;
 
+    //@Override
+    public Order save(Order order) {
+        return orderRepository.save(order);
+    }
+
+    //@Override
+    public Iterable<Order> findAll() {
+        return orderRepository.findAll();
+    }
+
+    //@Override
+    public Optional<Order> findById(Long id) {
+        return orderRepository.findById(id);
+    }
+
+    //@Override
+    public boolean existsById(Long id) {
+        return orderRepository.existsById(id);
+    }
+
+    //@Override
+    public void deleteById(Long id) {
+        orderRepository.deleteById(id);
+    }
     public OrderServiceImpl(CartRepository cartRepository, OrderRepository orderRepository) {
         this.cartRepository = cartRepository;
         this.orderRepository = orderRepository;
